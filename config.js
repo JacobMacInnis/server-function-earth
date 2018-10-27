2b6a924bba292aef031d5065cd23f6ac1e50fae0
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-10-26 21:54:55 -0400

    changed mongo database from functionearth to function-earth

--- config.js
+++ config.js
@@ -3,9 +3,11 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/functionearth',
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/functionearth-test',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/function-earth',
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/function-earth-test',
   CLIENT_ORIGIN: process.env.NODE_ENV || 'http://localhost:3000' || 'http://10.1.10.156:19001' || 'http://localhost:19000',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
 };
+
+
