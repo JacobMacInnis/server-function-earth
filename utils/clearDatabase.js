e7537ed3c2d2aaa250fc49f6dc661378a90e983c
Jacob MacInnis <jacobcmacinnis82@gmail.com>
2018-11-29 16:35:30 -0500

    fixed clearDatabase.js to drop all collections

--- utils/clearDatabase.js
+++ utils/clearDatabase.js
@@ -8,10 +8,11 @@ const User = require('../models/user');
 const UserStats = require('../models/user-stats');
 
 mongoose.connect(MONGODB_URI)
-  .then(() => mongoose.connection.collections['users'].drop( function(err) {
-    console.log('User collection dropped');
-  }))
-  .then(() => mongoose.disconnect())
+  .then(() => mongoose.connection.db.dropDatabase())
+  .then(() => {
+    console.log('function-earth db dropped');
+    mongoose.disconnect();
+  })
   .catch(err => {
     console.log(err);
   });
